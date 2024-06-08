1016a1fa80bf16a9d01ee01790079cef1b6ce24f
abhi-saini1 <abhishek977933@gmail.com>
2024-06-08 12:59:43 +0530

    commit

--- src/Components/EditTodo.tsx
+++ src/Components/EditTodo.tsx
@@ -4,13 +4,54 @@ import Input from "./Ui/Input"
 import Form from "./Ui/Form"
 import Button from "./Ui/Button"
 import { FiEdit } from "react-icons/fi";
+import { useState } from "react"
 
 
-const EditTodo = ({todo}:types) => {
-    const [first, setfirst] = useState(second)
+const EditTodo = ({todo}:{todo:types}) => {
+    const [editTodo, setEditTodo] = useState(false);
+
+    const handleEdit = () =>{
+      setEditTodo(!editTodo)
+    };
+
+    const handleSubmit = () =>{
+      setEditTodo(false);
+    }
   return (
-    <div>EditTodo</div>
-  )
-}
+
+
+    <div className="flex gap-5 items-center">
+      <Button onClick={handleSubmit}
+      text={<FiEdit/>}
+      actionButton/>
+
+      {editTodo ? (
+        <Form
+        action={edit}
+        onSubmit={handleSubmit}
+        >
+          <Input
+          name="inputId"
+          value={todo.id}
+          type="hidden"
+          />
+          <div className="flex justify-center">
+            <Input
+            type="text"
+            name="newTitle"
+            placeholder="Edit Todo..."
+            />
+            <Button type="submit" text='Save'/>
+          </div>
+        </Form>
+      ) : null}
+        
+      
+      
+    </div>
+    
+      
+    )
+  }
 
 export default EditTodo
